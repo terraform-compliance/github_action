3d3a2fe004543eae7c54a1f0481de34f3209411a
Emre Erkunt <emre.erkunt@gmail.com>
2020-10-14 13:52:03 +0100

    .

--- run.sh
+++ run.sh
@@ -27,15 +27,8 @@ if [[ -n $6 ]]; then
     PARAMETERS+=" -i \"$6\""
 fi
 
-echo "Total number of lines in the plan (before): $(cat $PLAN_FILE | wc -l)"
-echo " ====================== PLAN FILE (before) ========================= "
-cat "$PLAN_FILE" | head -n 1
-echo " ====================== PLAN FILE (before) ========================= "
-cat "$PLAN_FILE" | tail -n 1 > "$PLAN_FILE.tmp" && mv "$PLAN_FILE.tmp" "$PLAN_FILE"
-echo "Total number of lines in the plan (after): $(cat $PLAN_FILE | wc -l)"
-echo " ====================== PLAN FILE (after) ========================= "
-cat $PLAN_FILE
-echo " ====================== PLAN FILE (after) ========================= "
-
+ls -al "$PLAN_FILE"
+file "$PLAN_FILE"
 echo "Running: $PARAMETERS"
+
 $PARAMETERS
